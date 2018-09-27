7440ad52defbaa418e838a8385b5e6a4472a2524
leezaweb <leeza.web@gmail.com>
2018-09-27 13:40:09 -0400

    add citation.js to bundle. add condition on author list

--- assets/_js/bundle.js
+++ assets/_js/bundle.js
@@ -1,9 +1,11 @@
 import SiteHeader from './site-header'
 import MainMenu from './main-menu'
 import HeaderSearch from './header-search'
+import Citation from './citation'
 
 window.addEventListener('DOMContentLoaded', () => {
   SiteHeader()
   MainMenu()
   HeaderSearch()
+  Citation()
 })
