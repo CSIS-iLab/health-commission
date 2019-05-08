957f9da2e5bb93b8c6c63e6dfa101281e991acae
leezaweb <leeza.web@gmail.com>
2019-05-08 10:01:25 -0400

    put in the lightbox

--- assets/_js/bundle.js
+++ assets/_js/bundle.js
@@ -2,10 +2,12 @@ import SiteHeader from './site-header'
 import MainMenu from './main-menu'
 import HeaderSearch from './header-search'
 import Citation from './citation'
+import Lightbox from './lightbox'
 
 window.addEventListener('DOMContentLoaded', () => {
   SiteHeader()
   MainMenu()
   HeaderSearch()
   Citation()
+  Lightbox()
 })
