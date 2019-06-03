7acba382ad6d6ce5ab9bda4e1b82c2d4eefbb725
Jacque Schrag <jnschrag@gmail.com>
2019-06-03 17:16:59 -0400

    Begin upgrade to Gulp v4

--- _plugins/responsive-images.rb
+++ _plugins/responsive-images.rb
@@ -3,7 +3,7 @@ Jekyll::Hooks.register :documents, :pre_render do |post, payload|
   docExt = post.extname.tr('.', '')
   # only process if we deal with a markdown file
   if payload['site']['markdown_ext'].include? docExt
-    newContent = post.content.gsub(/\!\[(.*)?\]\((.*?)(?=\"|\))(\".*\")?\)/, '{% include cloudinary-responsive-img.html title=\3 alt="\1" path="\2" %}')
+    newContent = post.content.gsub(/\!\[(.*)?\]\((.*?)(?=\"|\'|\))((?:\"|\').*(?:\"|\'))?\)/, '{% include cloudinary-responsive-img.html title=\3 alt="\1" path="\2" %}')
     post.content = newContent
   end
 end
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,127 @@
-'use strict';
+const argv = require('yargs').argv
+const config = require('./frasco.config.js')
+const { src, dest, watch, series, parallel } = require('gulp')
+const autoprefixer = require('autoprefixer')
+const browserSync = require('browser-sync')
+const cp = require('child_process')
+const eslint = require('gulp-eslint')
+const postcss = require('gulp-postcss')
+const sass = require('gulp-sass')
+const log = require('fancy-log')
 
-const config     = require('./frasco.config.js');
-const gulp       = require('gulp');
-const requireDir = require('require-dir');
+const server = browserSync.create()
+const jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll'
 
-requireDir('./gulp_tasks', {recurse: true});
+const runEslint = function() {
+  return src([
+    config.assets + '/' + config.js.src + '/**/*.js',
+    '!node_modules/**'
+  ])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError())
+}
+exports.eslint = runEslint
 
-const tasks = [];
-Object.keys(config.tasks).forEach(function (key) {
-  if (config.tasks[key] && key != 'eslint') {
-    tasks.push((key == 'webpack' && config.tasks.watch) ? '_' + key : key);
+function sassTask() {
+  return src(config.assets + '/' + config.sass.src + '/**/*.scss')
+    .pipe(
+      sass({ outputStyle: config.sass.outputStyle }).on('error', sass.logError)
+    )
+    .pipe(postcss([autoprefixer(config.sass.autoprefixer)]))
+    .pipe(dest(config.assets + '/' + config.sass.dest))
+    .pipe(server.stream({ match: '**/*.css' }))
+    .on('end', () => log('SASS updated'))
+}
+
+/*----------  Browsersync  ----------*/
+
+let browser =
+  config.browsersync.browsers[0] != null
+    ? config.browsersync.browsers
+    : 'default'
+
+function reload(done) {
+  server.reload()
+  done()
+}
+
+function serve(done) {
+  server.init({
+    port: config.port,
+    browser: browser,
+    server: {
+      baseDir: config.jekyll.dest
+    }
+  })
+  done()
+}
+
+/*----------  Watch  ----------*/
+
+const filesToWatch = [
+  '!./node_modules/**/*',
+  '!./README.md',
+  '!' + config.jekyll.dest + '/**/*',
+  '_config*.yml',
+  '*.html',
+  './**/*.md',
+  './**/*.markdown',
+  '*.json',
+  config.jekyll.includes + '/**/*',
+  config.jekyll.layouts + '/**/*',
+  config.jekyll.posts + '/**/*',
+  config.assets + '/' + config.sass.dest + '/**/*',
+  config.assets + '/' + config.js.dest + '/**/*',
+  config.assets + '/' + config.imagemin.dest + '/**/*'
+]
+
+function watchTask() {
+  watch(
+    config.assets + '/' + config.sass.src + '/**/*',
+    series(sassTask, jekyllBuild, reload)
+  )
+
+  if (config.tasks.browsersync) {
+    watch(filesToWatch, series(jekyllBuild, reload))
   }
-});
+}
 
-/**
- * Default task, running just `gulp` will minify the images,
- * compile the sass, bundle the js, launch BrowserSync, and
- * watch files.
- */
-gulp.task('default', tasks);
+/*----------  Jekyll  ----------*/
+
+function jekyllBuild(done) {
+  let jekyllConfig = config.jekyll.config.default
+  if (argv.jekyllEnv == 'production') {
+    process.env.JEKYLL_ENV = 'production'
+    jekyllConfig += config.jekyll.config.production
+      ? ',' + config.jekyll.config.production
+      : ''
+  } else {
+    jekyllConfig += config.jekyll.config.development
+      ? ',' + config.jekyll.config.development
+      : ''
+  }
+
+  let buildArgs = ['build', '--config', jekyllConfig]
+
+  if (argv.preview) {
+    buildArgs.push('--drafts')
+    buildArgs.push('--unpublished')
+    buildArgs.push('--future')
+  }
+
+  if (argv.forestry) {
+    buildArgs.push('--destination')
+    buildArgs.push('_forestry_site')
+  }
+
+  console.log(buildArgs)
+
+  return cp
+    .spawn(jekyll, buildArgs, { stdio: 'inherit', env: process.env })
+    .on('close', done)
+}
+
+exports.default = series(sassTask, jekyllBuild, serve, watchTask)
+
+exports.build = series(parallel(sassTask), jekyllBuild)
